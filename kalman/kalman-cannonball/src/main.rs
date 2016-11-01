4050288225585efdc7e9d59c9bb4364867ea759b
Gary Evans <Gary Evans>
2016-11-01 21:30:43 +0000

    change to state-space form

--- kalman/kalman-cannonball/src/main.rs
+++ kalman/kalman-cannonball/src/main.rs
@@ -11,7 +11,8 @@ fn main() {
 
     let angle = 45.0;
     let muzzle_velocity = 100.0;
-    let gravity = Vector2::new(0.0, -9.81); // [0,-9.81];
+    let g = -9.81;
+    let gravity = Vector2::new(0.0, g);
     let initial_velocity_x = muzzle_velocity * (angle * f64::consts::PI/180.0).cos();
     let initial_velocity_y = muzzle_velocity * (angle * f64::consts::PI/180.0).sin();
     let initial_velocity = Vector2::new(initial_velocity_x, initial_velocity_y);
@@ -24,10 +25,43 @@ fn main() {
     let mut velocity = initial_velocity;
     let mut acceleration = initial_acceleration;
 
-    for x in 0..140 {
-        acceleration = gravity;
-        velocity += acceleration * delta_time;
-        location += velocity * delta_time;
-        println!("{} {} {}", x, location[0], location[1]);
+    let A = Matrix4::new(0.0, 1.0, 0.0, 0.0,
+                         0.0, 0.0, 0.0, 0.0,
+                         0.0, 0.0, 0.0, 1.0,
+                         0.0, 0.0, 0.0, 0.0);
+
+    // TODO: really want B to be a a 4x1 matrix, and u to be a scalar instead of a 4x1 vector
+    //  - not present in nalgebra ?!
+    let B = Matrix4::new(0.0, 0.0, 0.0, 0.0,
+                         0.0, 0.0, 0.0, 0.0,
+                         0.0, 0.0, 0.0, 0.0,
+                         0.0, 0.0, 0.0, 1.0);
+
+    let u = Vector4::new(0.0, 0.0, 0.0, g);
+
+    let mut state = Vector4::new(initial_location[0], initial_velocity[0], initial_location[1], initial_velocity[1]);
+
+    for x in 0..145 {
+        // acceleration = gravity;
+        // velocity += acceleration * delta_time;
+        // location += velocity * delta_time;
+        // let x_loc = location[0];
+        // let y_loc = location[1]; 
+        let state_dot = (A * state) + (B * u);
+        if (x == 0)
+        {
+            println!("{}", state);
+            println!("{}", state_dot);
+        }
+        state += (state_dot * delta_time);
+        if (x == 0)
+        {
+            println!("{}", state);
+        }
+
+        let x_loc = state[0];
+        let y_loc = state[2];
+
+        println!("{} {} {}", x, x_loc, y_loc);
     }
 }
